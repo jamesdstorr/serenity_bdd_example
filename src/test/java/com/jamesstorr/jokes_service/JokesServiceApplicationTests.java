2d12bc7fc1907f667b3ec4351bc4bc2b4d0772dc
jamesdstorr <jamesstorr@live.co.uk>
2024-09-20 14:47:50 +0100

    Recreated

---
+++ src/test/java/com/jamesstorr/jokes_service/JokesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesstorr.jokes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
